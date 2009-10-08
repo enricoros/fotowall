aa4986784bb93a5c72abe2f753f90b241f8d87af
TANGUY Arnaud <arn.tanguy@gmail.com>
2009-10-08 21:33:32 +0200

    Relicence my files GPL v2+
    They were mainly gpl v2+, but now all the headers are homogeneous.

--- Shared/CroppingWidget.h
+++ Shared/CroppingWidget.h
@@ -1,20 +1,16 @@
-/******************************************************************************
-     Copyright (C) 2008  TANGUY Arnaud arn.tanguy@gmail.com
-*                                                                             *
-* This program is free software; you can redistribute it and/or modify        *
-* it under the terms of the GNU General Public License as published by        *
-* the Free Software Foundation; either version 2 of the License, or           *
-* (at your option) any later version.                                         *
-*                                                                             *
-* This program is distributed in the hope that it will be useful,             *
-* but WITHOUT ANY WARRANTY; without even the implied warranty of              *
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                *
-* GNU General Public License for more details.                                *
-*                                                                             *
-* You should have received a copy of the GNU General Public License along     *
-* with this program; if not, write to the Free Software Foundation, Inc.,     *
-* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.                 *
- ******************************************************************************/
+/***************************************************************************
+ *                                                                         *
+ *   This file is part of the Fotowall project,                            *
+ *       http://www.enricoros.com/opensource/fotowall                      *
+ *                                                                         *
+ *   Copyright (C) 2009 by TANGUY Arnaud <arn.tanguy@gmail.com>            *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
 #ifndef __CroppingWidget__
 #define __CroppingWidget__
