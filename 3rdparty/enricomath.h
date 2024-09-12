0d864e517f4a075aeb4549eb5be67a3e8be4b016
Arnaud TANGUY <arn.tanguy@gmail.com>
2024-09-12 10:38:26 +0200

    Remove sincos

--- 3rdparty/enricomath.h
+++ 3rdparty/enricomath.h
@@ -286,17 +286,6 @@ class Matrix3
 #define M_PI 3.141592653589793238462643
 #endif // !M_PI
 
-#if !defined(Q_CC_GNU) || defined(Q_OS_WIN32) || defined(Q_OS_OS2) || defined(Q_OS_ANDROID)
-#if !defined(__MINGW32__)
-//sincos is not defined in win32, MAC, OS/2 and Android
-static inline void sincos(double th, double *s, double *c)
-{
-    *s = sin(th);
-    *c = cos(th);
-}
-#endif
-#endif
-
 /****************************************************************************
  Vector2 - inline functions
  ****************************************************************************/
